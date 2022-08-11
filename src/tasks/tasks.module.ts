75cc7a0cfc219747abf3b1930efb7a89d3859529
Edidong Ekpenyong <108550947+eddiEkpenyong@users.noreply.github.com>
2022-08-11 12:48:12 +0100

    updated_tasksService_tasksController_with_getAllTaks

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { TasksController } from './tasks.controllers';
+import { TaskService } from './tasks.service';
 
 @Module({
     imports: [],
-    providers: [],
+    providers: [TasksService],
     controllers: [TasksController]
 })
 export class TasksModule{}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TasksService{
+   private tasks = []
+
+   getAllTasks(): tasks[]{
+    return this.tasks
+   }
+   
+}
